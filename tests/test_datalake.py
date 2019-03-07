39e89eb122b43b32bcbadefb052ef151ceb8e901
Fabian Wollert <fabian@zalando.de>
2019-03-07 14:33:37 +0100

    create datalake plugin

---
+++ tests/test_datalake.py
@@ -0,0 +1,49 @@
+from zmon_worker_monitor.builtins.plugins.datalake import DatalakeWrapper
+from zmon_worker_monitor.zmon_worker.common.http import get_user_agent
+
+from mock import MagicMock
+
+
+def resp_mock(failure=False):
+    resp = MagicMock()
+    resp.ok = True if not failure else False
+    json_res = {
+        "id": "abc",
+        "limit": 10000,
+        "offset": 0,
+        "result": [],
+        "truncated": False
+    } if not failure else {'message': "failed!", 'id': "def"}
+    resp.json.return_value = json_res
+
+    return resp
+
+
+def requests_mock(resp, failure=None):
+    req = MagicMock()
+
+    if failure is not None:
+        req.side_effect = failure
+    else:
+        req.return_value = resp
+
+    return req
+
+
+def test_elasticsearch_search(monkeypatch):
+    resp = resp_mock()
+    get = requests_mock(resp)
+    monkeypatch.setattr('requests.get', get)
+
+    url = 'http://datalake/'
+    dl = DatalakeWrapper(url)
+
+    q = 'SELECT * FROM dummy_table LIMIT 1'
+    result = dl.query(query=q)
+
+    assert result == resp.json.return_value
+
+    get.assert_called_with('http://datalake/jobs',
+                           headers={'User-Agent': get_user_agent()},
+                           params={'query': q},
+                           timeout=10)
---
+++ zmon_worker_monitor/builtins/plugins/datalake.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import requests
+import logging
+import time
+import tokens
+
+from zmon_worker_monitor.zmon_worker.errors import ConfigurationError, CheckError
+from zmon_worker_monitor.adapters.ifunctionfactory_plugin import IFunctionFactoryPlugin, propartial
+
+logger = logging.getLogger('zmon-worker.scalyr-function')
+
+QUERY_STATUS_INTERVAL = 5
+
+# will use OAUTH2_ACCESS_TOKEN_URL environment variable by default
+# will try to read application credentials from CREDENTIALS_DIR
+tokens.configure()
+tokens.manage('uid', ['uid'])
+tokens.start()
+
+
+class DatalakeFactory(IFunctionFactoryPlugin):
+    def __init__(self):
+        super(DatalakeFactory, self).__init__()
+        self._url = None
+
+    def configure(self, conf):
+        """
+        Called after plugin is loaded to pass the [configuration] section in their plugin info file
+        :param conf: configuration dictionary
+        """
+        self._url = conf.get('url')
+
+    def create(self, factory_ctx):
+        """
+        Automatically called to create the check function's object
+        :param factory_ctx: (dict) names available for Function instantiation
+        :return: an object that implements a check function
+        """
+        return propartial(DatalakeWrapper, url=self._url)
+
+
+class DatalakeWrapper(object):
+    def __init__(self, url, oauth2=False):
+        if not url:
+            raise ConfigurationError('Datalake wrapper improperly configured. URL is missing!')
+
+        self.url = url
+
+        self.__session = requests.Session()
+
+        if oauth2:
+            self.__session.headers.update({'Authorization': 'Bearer {}'.format(tokens.get('uid'))})
+
+    def query(self, query):
+        # submit query first
+        job_id = self.__session.post('{}/jobs'.format(self.url), json=query).json()['id']
+
+        # check if query has finished
+        while self.__session.get('{}/jobs/{}'.format(self.url, job_id), json=query).json()['status'] == 'RUNNING':
+            time.sleep(QUERY_STATUS_INTERVAL)
+
+        # check for failures
+        job_status = self.__session.get('{}/jobs/{}'.format(self.url, job_id), json=query).json()
+        if job_status['status'] != 'FINISHED':
+            logs = self.__session.get('{}/jobs/{}/logs'.format(self.url, job_id), json=query).json()
+            raise CheckError('Datalake query failed! error message: {}'.format(logs['message']))
+        else:
+            # return result
+            return self.__session.get('{}/jobs/{}/output'.format(self.url, job_id), json=query).json()
